6db140ed07954ba073ebcb8ba3cfda233c70cc14
LETDesignWorks <michael@letdw.com>
2026-09-10 21:08:15 -0600

    Refresh automatic address lookup on the bus access map

--- bus-access/app.js
+++ bus-access/app.js
@@ -1,4 +1,4 @@
-await import('../assets/location-address-autofill.js?v=20260910-1');
+await import('../assets/location-address-autofill.js?v=20260910-2');
 
 const enhancementStyles=document.createElement('link');
 enhancementStyles.rel='stylesheet';
